c11f9bfb40b692f9708611ca60e20398b4985493
Abdullah Abdulsattar <abdullah_a_s@hotmail.co.uk>
2022-02-10 22:18:50 +0000

    improved functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,82 @@
 import React, { useEffect, useState } from "react";
-import './App.scss';
+import "./App.scss";
 import Main from "./containers/Main/Main";
 import beers from "./data/beers";
 import Navbar from "./containers/Navbar/Navbar";
 
 const App = () => {
-
   const [searchTerm, setSearchTerm] = useState("");
-  const [beer, setBeer] = useState();
+  const [beerArr, setBeerArr] = useState([]);
   const [checkedAbv, setCheckedAbv] = useState(false);
-  const [beerAbv, setBeerAbv] = useState([]);
   const [checkedDate, setCheckedDate] = useState(false);
-  const [beerDate, setBeerDate] = useState([]);
   const [checkedPh, setCheckedPh] = useState(false);
-
-
+  const [abv, setAbv] = useState("");
+  const [dateBrewed, setDateBrewed] = useState("");
 
   useEffect(() => {
-    const URL = `https://api.punkapi.com/v2/beers/${searchTerm}`
+    if (checkedAbv && checkedDate) {
+      setAbv("&abv_gt=6");
+      setDateBrewed("?brewed_before=01-2010");
+    } else if (checkedAbv) {
+      setAbv("?abv_gt=6");
+    } else if (checkedDate) {
+      setDateBrewed("?brewed_before=01-2010");
+    } else {
+      setAbv("");
+      setDateBrewed("");
+    }
+    const URL = `https://api.punkapi.com/v2/beers${dateBrewed}${abv}`;
     fetch(URL)
-      .then(response => response.json())
-      .then(beer => {
-        setBeer(beer);
+      .then((response) => response.json())
+      .then((beers) => {
+        if (checkedPh) {
+          setBeerArr(beers.filter(beer => {return beer.ph < 4 && beer.name.toLowerCase().includes(searchTerm)}))
+        } else {
+          setBeerArr(beers.filter(beer => {return beer.name.toLowerCase().includes(searchTerm)}));
+        }
       });
-
-  }, [searchTerm])
+  }, [abv, dateBrewed, searchTerm, checkedAbv, checkedDate, checkedPh]);
 
   const handleInput = (event) => {
     console.log(event);
-    setSearchTerm(event.target.value)
-  }
-
-  console.log(beer);
-
-  useEffect(() => {
-    const URL = `https://api.punkapi.com/v2/beers?abv_gt=6`
-    fetch(URL)
-      .then(response => response.json())
-      .then(beer => {
-        setBeerAbv(beer);
-      });
-  }, [])
-
-  console.log(beerAbv);
-
-  useEffect(() => {
-    const URL = `https://api.punkapi.com/v2/beers?brewed_before=01-2010`
-    fetch(URL)
-      .then(response => response.json())
-      .then(beer => {
-        setBeerDate(beer);
-      });
-  }, [])
-
+    setSearchTerm(event.target.value);
+  };
 
   const handleAbvClick = (event) => {
-    console.log(event)
-    setCheckedAbv(event.target.checked)
-  }
+    console.log(event);
+    setCheckedAbv(event.target.checked);
+  };
 
   const handleDateClick = (event) => {
-    console.log(event)
-    setCheckedDate(event.target.checked)
-  }
-  
+    console.log(event);
+    setCheckedDate(event.target.checked);
+  };
+
   const handlePhClick = (event) => {
-    console.log(event)
-    setCheckedPh(event.target.checked)
-  }
+    console.log(event);
+    setCheckedPh(event.target.checked);
+  };
 
   return (
     <>
       <div className="App">
         <section className="navbar">
-          <Navbar searchTerm={searchTerm} handleInput={handleInput} handleAbvClick={handleAbvClick} handleDateClick={handleDateClick} handlePhClick={handlePhClick} />
+          <Navbar
+            searchTerm={searchTerm}
+            handleInput={handleInput}
+            handleAbvClick={handleAbvClick}
+            handleDateClick={handleDateClick}
+            handlePhClick={handlePhClick}
+          />
         </section>
         <section key={beers.id} className="main">
-          <Main searchTerm={searchTerm} beerAbv={beerAbv} beerDate={beerDate} checkedAbv={checkedAbv} checkedDate={checkedDate} checkedPh={checkedPh} />
+          <Main
+            filteredBeers={beerArr}
+          />
         </section>
       </div>
     </>
   );
-}
+};
 
 export default App;
-
-// checkedAbv={checkedAbv} checkedDate={checkedDate} checkedPh={checkedPh}
--- src/containers/Main/Main.jsx
+++ src/containers/Main/Main.jsx
@@ -1,59 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./Main.scss";
 import Card from "../../components/Card/Card";
 // import CardList from "../CardList/CardList";
 
 const Main = (props) => {
-  const { searchTerm, beerAbv, beerDate, checkedAbv, checkedDate, checkedPh } = props;
+  const { filteredBeers } = props;
 
-  const [beersArr, setBeersArr] = useState([]);
-
-  useEffect(() => {
-    const URL = "https://api.punkapi.com/v2/beers";
-    fetch(URL)
-      .then((response) => response.json())
-      .then((beer) => {
-        console.log(beer);
-        setBeersArr(beer);
-      });
-  }, []);
-
-  const filteredBeers = beersArr.filter((beer) => {
-    const beerName = beer.name.toLowerCase();
-    console.log("searchTerm =", searchTerm);
-    console.log("Beer name includes=", beerName.includes(searchTerm));
-    return beerName.includes(searchTerm);
-  });
-
-  const filteredPhBeers = beersArr.filter((beer) => {
-    return beer.ph < 4;
-  });
-
-  const beerAbvJSX = beerAbv.map((beer, index) => {
-    return (
-      <>
-        <Card
-          key={"beer" + index}
-          img={beer.image_url}
-          name={beer.name}
-          tagline={beer.tagline}
-        />
-      </>
-    );
-  });
-
-  const beerDateJSX = beerDate.map((beer) => {
-    return (
-      <>
-        <Card
-          key={beer.id}
-          img={beer.image_url}
-          name={beer.name}
-          tagline={beer.tagline}
-        />
-      </>
-    );
-  });
+  console.log(filteredBeers);
 
   const beerCardJSX = filteredBeers.map((beer, index) => {
     return (
@@ -68,41 +21,10 @@ const Main = (props) => {
     );
   });
 
-  const beerPhJSX = filteredPhBeers.map((beer, index) => {
-    return (
-      <>
-        <Card
-          key={"beer" + index}
-          img={beer.image_url}
-          name={beer.name}
-          tagline={beer.tagline}
-        />
-      </>
-    );
-  });
-
-  const displayCards = () => {
-    if (checkedAbv) {
-      return beerAbvJSX;
-    } else if (checkedDate) {
-      return beerDateJSX;
-    } else if (checkedPh) {
-      return beerPhJSX;
-    } else {
-      return beerCardJSX;
-    }
-  };
-
-  //make another jsx variable that maps over a second filteredbbers array that only contains that abv beers array and also returns a card component with the right props, then create a function that produces an if statement where if checked is true then return the second jsx variable whereas if false the return the first jsx variable
-
-  console.log(filteredBeers);
-
-  console.log(beerCardJSX);
 
   return (
     <>
-      {/* <div className="container">{displayCards()}</div> */}
-      {displayCards()}
+      {beerCardJSX}
     </>
   );
 };
